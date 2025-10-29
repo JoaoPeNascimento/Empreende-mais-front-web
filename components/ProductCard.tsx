b77e518f766250077813f34f61fa6dc57f391585
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-10-29 20:27:33 -0300

    Inital commit

---
+++ components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "./ui/card";
+
+const ProductCard = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h1>Roupa Bonita</h1>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
